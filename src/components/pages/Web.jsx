b971ce338f3f605023059a3ce5895a7746e27044
Angel Calderon <acalderono@gmail.com>
2021-09-07 18:54:54 -0300

    add styled

--- src/components/pages/Web.jsx
+++ src/components/pages/Web.jsx
@@ -17,6 +17,14 @@ import {
   List,
   ProjectName,
   ProjectHour,
+  Table,
+  THead,
+  TBody,
+  Tr,
+  Th,
+  Td,
+  Project,
+  Letter,
 } from "./Styles";
 
 export const isoCurrentWeekAndYear = () => {
@@ -57,6 +65,9 @@ export const Web = () => {
           .reduce((a, b) => a + b, 0);
 
         const projects = data.filter((o) => o.assignment === assig);
+        // .map((o) => o.hours)
+        // .reduce((a, b) => a + b, 0);
+        console.log(projects);
 
         let addNew = {
           assignment: assig,
@@ -69,6 +80,7 @@ export const Web = () => {
 
       setCustomers(newAssignment);
       setLoading(false);
+      console.log(newAssignment);
     };
     getFetchCustomer();
 
@@ -237,7 +249,113 @@ const Card = (props) => {
   );
 };
 
+const TableSection = () => {
+  const [users, setUsers] = useState();
+  const [isLoading, setLoading] = useState(true);
+  const titles = ["Equipo", "Nombre", "Proyecto", "Hora"];
+
+  const { REACT_APP_HOST_URL } = process.env;
+
+  useEffect(() => {
+    const abortCtrl = new AbortController();
+    const signal = abortCtrl.signal;
+
+    const getFetch = async () => {
+      let r = await fetch(`${REACT_APP_HOST_URL}/api/team/7`, {
+        method: "GET",
+        signal,
+      });
+
+      let { data } = await r.json().then();
+      let list = data.map((o) => ({
+        team: o.team,
+        initial: getFirstCharacter(o.firstname).concat(
+          getFirstCharacter(o.lastname)
+        ),
+        username: o.user,
+        firstname: o.firstname,
+        lastname: o.lastname,
+        assignment: o.assignment,
+        customer: o.customer,
+        hours: o.hours,
+        year: o.year,
+        week: o.week,
+        project: o.project,
+      }));
+
+      console.log(groupBy(data, "user"));
+
+      setUsers(list);
+      setLoading(false);
+      console.log(list);
+    };
+
+    getFetch();
+
+    return () => {
+      // signal.abort();
+      console.log("dismount card");
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <ContentTitle>Cargando...</ContentTitle>
+      ) : (
+        <Table>
+          <THead>
+            <Tr>
+              {titles.map((t, i) => (
+                <Th key={i}>{t}</Th>
+              ))}
+            </Tr>
+          </THead>
+          <TBody>
+            {users.map((u, i) => (
+              <Tr key={i}>
+                <Td>{u.team}</Td>
+                <Td>
+                  <Letter letters={u.initial} color={"green"}></Letter>
+                  {u.firstname} {u.lastname}
+                </Td>
+                <Td>
+                  <Project>
+                    <span className="asigg">{u.assignment}</span>
+                    <span style={{ fontSize: "18px" }}>{u.project}</span>
+                    <span className="customer">{u.customer} </span>
+                  </Project>
+                </Td>
+                <Td>{u.hours}</Td>
+              </Tr>
+            ))}
+          </TBody>
+        </Table>
+      )}
+    </>
+  );
+};
+
 const sortByHours = (data = []) => {
   return data.sort((a, b) => b.hours - a.hours);
 };
+
+const getColor = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+const getFirstCharacter = (string) => string.charAt(0).toUpperCase();
+const groupBy = (array, key) => {
+  // Return the end result
+  return array.reduce((result, currentValue) => {
+    // If an array already present for key, push it to the array. Else create an array and push the object
+    (result[currentValue[key]] = result[currentValue[key]] || []).push(
+      currentValue
+    );
+    // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+    return result;
+  }, {}); // empty object is the initial value for result object
+};
+
 export default Web;
