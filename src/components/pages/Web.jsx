02956d783b2f479c25a11cb0a5de25dad38c098f
Angel Calderon Oyarce <acalderon@activeit.cl>
2022-04-13 16:23:12 -0400

    se agrega filtro

--- src/components/pages/Web.jsx
+++ src/components/pages/Web.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import * as moment from "moment";
-import { ReactComponent as KeySolid } from "./../assets/key-solid.svg";
-import { ReactComponent as AitLogo } from "./../assets/ait-logo.svg";
+import React, { useState, useEffect } from 'react';
+import * as moment from 'moment';
+import { ReactComponent as KeySolid } from './../assets/key-solid.svg';
+import { ReactComponent as AitLogo } from './../assets/ait-logo.svg';
 import {
   Main,
   Section,
@@ -17,15 +17,7 @@ import {
   List,
   ProjectName,
   ProjectHour,
-  Table,
-  THead,
-  TBody,
-  Tr,
-  Th,
-  Td,
-  Project,
-  Letter,
-} from "./Styles";
+} from './Styles';
 
 export const isoCurrentWeekAndYear = () => {
   const currentWeek = moment().isoWeek();
@@ -39,6 +31,7 @@ export const Web = () => {
   const { REACT_APP_HOST_URL } = process.env;
   const { currentWeek, currentYear } = isoCurrentWeekAndYear();
 
+  const [teamId, setTeamId] = useState(7);
   const [year, setYear] = useState(currentYear);
   const [week, setWeek] = useState(currentWeek);
 
@@ -47,9 +40,10 @@ export const Web = () => {
     const signal = abortCtrl.signal;
     const getFetchCustomer = async () => {
       let r = await fetch(
-        `${REACT_APP_HOST_URL}/team/7/customers?year=${year}&week=${week}`,
+        `${REACT_APP_HOST_URL}/team/${teamId}/customers?year=${year}&week=${week}`,
         {
-          method: "GET",
+          method: 'GET',
+          mode: 'cors',
           signal,
         }
       );
@@ -74,14 +68,14 @@ export const Web = () => {
           value.week === sortedData[index + 1].week
         ) {
           orderedData.push({
-            is_customer: value.assignment === "Cliente" ? true : false,
+            is_customer: value.assignment === 'Cliente' ? true : false,
             assignment: value.assignment,
             customer: value.customer,
             hours: value.hours + sortedData[index + 1].hours,
           });
         } else {
           orderedData.push({
-            is_customer: value.assignment === "Cliente" ? true : false,
+            is_customer: value.assignment === 'Cliente' ? true : false,
             assignment: value.assignment,
             customer: value.customer,
             hours: value.hours,
@@ -101,7 +95,7 @@ export const Web = () => {
         const projects = result.filter((o) => o.assignment === assig);
         let addNew = {
           assignment: assig,
-          is_customer: assig === "Cliente" ? true : false,
+          is_customer: assig === 'Cliente' ? true : false,
           hours: hoursOfAssignment,
           projects: sortByHours(projects),
         };
@@ -116,10 +110,10 @@ export const Web = () => {
     return () => {
       // signal.abort();
       setLoading(false);
-      console.log("cleaned up");
+      console.log('cleaned up');
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [REACT_APP_HOST_URL, week, year]);
+  }, [REACT_APP_HOST_URL, week, year, teamId]);
 
   const MiniCard = (props) => {
     return (
@@ -156,19 +150,19 @@ export const Web = () => {
     );
   };
 
-  const hanleChangeWeek = (event) => {
+  const handleChangeWeek = (event) => {
     setWeek(event.target.value);
   };
 
   const SelectWeek = () => {
     const weeksList = [
-      { week: currentWeek - 1, name: "última semana" },
-      { week: currentWeek, name: "semana actual" },
+      { week: currentWeek - 1, name: 'última semana' },
+      { week: currentWeek, name: 'semana actual' },
     ];
     return (
       <label>
         Fecha
-        <select onChange={hanleChangeWeek} defaultValue={week}>
+        <select onChange={handleChangeWeek} defaultValue={week}>
           {weeksList.map((wk, i) => (
             <option key={i} value={wk.week}>
               {wk.name}
@@ -179,25 +173,92 @@ export const Web = () => {
     );
   };
 
+  const handleChangeTeam = (event) => {
+    console.log(event.target.value);
+    setTeamId(event.target.value);
+  };
+
+  const SelectTeam = () => {
+    const teamList = [
+      {
+        id_team: 52,
+        team: 'Fabrica Siebel (CL)',
+        status: 'Activo',
+      },
+      {
+        id_team: 53,
+        team: 'Infraestructura',
+        status: 'Activo',
+      },
+      {
+        id_team: 7,
+        team: 'Fabrica Desarrollo',
+        status: 'Activo',
+      },
+      {
+        id_team: 59,
+        team: 'Fabrica Siebel (CO)',
+        status: 'Activo',
+      },
+      {
+        id_team: 60,
+        team: 'KAM',
+        status: 'Activo',
+      },
+      {
+        id_team: 104,
+        team: 'Grupo CO/DO',
+        status: 'Activo',
+      },
+    ];
+
+    return (
+      <label>
+        Equipo
+        <select onChange={handleChangeTeam} defaultValue={teamId}>
+          {teamList.map((item, i) => (
+            <option
+              key={i}
+              value={item.id_team}
+              disabled={item.status !== 'Activo'}
+            >
+              {item.team}
+            </option>
+          ))}
+        </select>
+      </label>
+    );
+  };
+
   return (
     <>
       {isLoading ? (
-        "Cargando..."
+        'Cargando...'
       ) : (
         <>
           <div>
-            <SelectYear></SelectYear>
-            <SelectWeek></SelectWeek>
+            <SelectYear />
+            <SelectWeek />
+            <SelectTeam />
           </div>
           <Main>
             {!customers.length ? (
-              <div>Sin informacion</div>
+              <div>
+                No hay información, intenta más tarde o pide que imputen las HH
+                ;P
+              </div>
             ) : (
               customers.map((customer, i) => (
                 <Section key={i}>
                   <MiniCard key={i} {...customer}></MiniCard>
                   {customer.projects.map((project, j) => (
-                    <Card key={j} week={week} year={year} {...project} />
+                    <Card
+                      key={j}
+                      week={week}
+                      year={year}
+                      teamId={teamId}
+                      {...project}
+                    />
                   ))}
                 </Section>
               ))
@@ -212,6 +273,7 @@ export const Web = () => {
 const Card = (props) => {
   const [projects, setProjects] = useState();
   const [isLoading, setLoading] = useState(true);
+  const teamId = props.teamId;
   const customer = props.customer;
   const year = props.year;
   const week = props.week;
@@ -223,9 +285,10 @@ const Card = (props) => {
 
     const getFetch = async () => {
       let r = await fetch(
-        `${REACT_APP_HOST_URL}/team/7/projects/${customer}?year=${year}&week=${week}`,
+        `${REACT_APP_HOST_URL}/team/${teamId}/projects/${customer}?year=${year}&week=${week}`,
         {
-          method: "GET",
+          method: 'GET',
+          mode: 'cors',
           signal,
         }
       );
@@ -271,7 +334,7 @@ const Card = (props) => {
 
     return () => {
       // signal.abort();
-      console.log("dismount card");
+      console.log('dismount card');
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [customer, year, week]);
@@ -310,110 +373,110 @@ const Card = (props) => {
   );
 };
 
-const TableSection = () => {
-  const [users, setUsers] = useState();
-  const [isLoading, setLoading] = useState(true);
-  const titles = ["Equipo", "Nombre", "Proyecto", "Hora"];
+// const TableSection = () => {
+//   const [users, setUsers] = useState();
+//   const [isLoading, setLoading] = useState(true);
+//   const titles = ['Equipo', 'Nombre', 'Proyecto', 'Hora'];
 
-  const { REACT_APP_HOST_URL } = process.env;
+//   const { REACT_APP_HOST_URL } = process.env;
 
-  useEffect(() => {
-    const abortCtrl = new AbortController();
-    const signal = abortCtrl.signal;
+//   useEffect(() => {
+//     const abortCtrl = new AbortController();
+//     const signal = abortCtrl.signal;
 
-    const getFetch = async () => {
-      let r = await fetch(`${REACT_APP_HOST_URL}/api/team/7`, {
-        method: "GET",
-        signal,
-      });
+//     const getFetch = async () => {
+//       let r = await fetch(`${REACT_APP_HOST_URL}/api/team/7`, {
+//         method: 'GET',
+//         signal,
+//       });
 
-      let { data } = await r.json().then();
-      let list = data.map((o) => ({
-        team: o.team,
-        initial: getFirstCharacter(o.firstname).concat(
-          getFirstCharacter(o.lastname)
-        ),
-        username: o.user,
-        firstname: o.firstname,
-        lastname: o.lastname,
-        assignment: o.assignment,
-        customer: o.customer,
-        hours: o.hours,
-        year: o.year,
-        week: o.week,
-        project: o.project,
-      }));
+//       let { data } = await r.json().then();
+//       let list = data.map((o) => ({
+//         team: o.team,
+//         initial: getFirstCharacter(o.firstname).concat(
+//           getFirstCharacter(o.lastname)
+//         ),
+//         username: o.user,
+//         firstname: o.firstname,
+//         lastname: o.lastname,
+//         assignment: o.assignment,
+//         customer: o.customer,
+//         hours: o.hours,
+//         year: o.year,
+//         week: o.week,
+//         project: o.project,
+//       }));
 
-      setUsers(list);
-      setLoading(false);
-    };
+//       setUsers(list);
+//       setLoading(false);
+//     };
 
-    getFetch();
+//     getFetch();
 
-    return () => {
-      // signal.abort();
-      console.log("dismount card");
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//     return () => {
+//       // signal.abort();
+//       console.log('dismount card');
+//     };
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
-  return (
-    <>
-      {isLoading ? (
-        <ContentTitle>Cargando...</ContentTitle>
-      ) : (
-        <Table>
-          <THead>
-            <Tr>
-              {titles.map((t, i) => (
-                <Th key={i}>{t}</Th>
-              ))}
-            </Tr>
-          </THead>
-          <TBody>
-            {users.map((u, i) => (
-              <Tr key={i}>
-                <Td>{u.team}</Td>
-                <Td>
-                  <Letter letters={u.initial} color={"green"}></Letter>
-                  {u.firstname} {u.lastname}
-                </Td>
-                <Td>
-                  <Project>
-                    <span className="asigg">{u.assignment}</span>
-                    <span style={{ fontSize: "18px" }}>{u.project}</span>
-                    <span className="customer">{u.customer} </span>
-                  </Project>
-                </Td>
-                <Td>{u.hours}</Td>
-              </Tr>
-            ))}
-          </TBody>
-        </Table>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {isLoading ? (
+//         <ContentTitle>Cargando...</ContentTitle>
+//       ) : (
+//         <Table>
+//           <THead>
+//             <Tr>
+//               {titles.map((t, i) => (
+//                 <Th key={i}>{t}</Th>
+//               ))}
+//             </Tr>
+//           </THead>
+//           <TBody>
+//             {users.map((u, i) => (
+//               <Tr key={i}>
+//                 <Td>{u.team}</Td>
+//                 <Td>
+//                   <Letter letters={u.initial} color={'green'}></Letter>
+//                   {u.firstname} {u.lastname}
+//                 </Td>
+//                 <Td>
+//                   <Project>
+//                     <span className="asigg">{u.assignment}</span>
+//                     <span style={{ fontSize: '18px' }}>{u.project}</span>
+//                     <span className="customer">{u.customer} </span>
+//                   </Project>
+//                 </Td>
+//                 <Td>{u.hours}</Td>
+//               </Tr>
+//             ))}
+//           </TBody>
+//         </Table>
+//       )}
+//     </>
+//   );
+// };
 
 const sortByHours = (data = []) => {
   return data.sort((a, b) => b.hours - a.hours);
 };
 
-const getColor = () => {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-};
+// const getColor = () => {
+//   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// };
 
-const getFirstCharacter = (string) => string.charAt(0).toUpperCase();
-const groupBy = (array, key) => {
-  // Return the end result
-  return array.reduce((result, currentValue) => {
-    // If an array already present for key, push it to the array. Else create an array and push the object
-    (result[currentValue[key]] = result[currentValue[key]] || []).push(
-      currentValue
-    );
-    // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
-    return result;
-  }, {}); // empty object is the initial value for result object
-};
+// const getFirstCharacter = (string) => string.charAt(0).toUpperCase();
+// const groupBy = (array, key) => {
+//   // Return the end result
+//   return array.reduce((result, currentValue) => {
+//     // If an array already present for key, push it to the array. Else create an array and push the object
+//     (result[currentValue[key]] = result[currentValue[key]] || []).push(
+//       currentValue
+//     );
+//     // Return the current iteration `result` value, this will be taken as next iteration `result` value and accumulate
+//     return result;
+//   }, {}); // empty object is the initial value for result object
+// };
 
 export default Web;
